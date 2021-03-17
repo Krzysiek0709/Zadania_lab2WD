4d1cbe10e5b58078f4f19a10917aa6962e183c8a
Krzysiek0709 <trolek700@gmail.com>
2021-03-17 16:45:21 +0100

    Zadania WD 2

---
+++ Zadanie9.py
@@ -0,0 +1,14 @@
+import sys as system
+
+
+lista = [1,2,3,4,5,6]
+for x in range(5):
+    if x%2==0:
+        for y in range(lista[5]):
+            system.stdout.write("E")
+        print("\n")
+    else:
+        for y in range(lista[0]):
+            system.stdout.write("E")
+        print("\n")
+print("\n")
