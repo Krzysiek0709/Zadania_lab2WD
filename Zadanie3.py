ab12f23097ef5819e076966a7f8347f06ed75122
Krzysiek0709 <trolek700@gmail.com>
2021-03-17 16:35:48 +0100

    Zadania WD 2

---
+++ Zadanie3.py
@@ -0,0 +1,3 @@
+slownik_gier = {"swtor" : "Star Wars: The Old Republic", "lol" : "league of legends", "PUBG" : "Player Unknown: Battlegrounds", "GI" : "Genshin Impact"}
+print(len(slownik_gier))
+print("\n")
